8abe9a9085034d2fbe105062880b3c6db5ee84b3
wayzeng <wayzeng@tencent.com>
2021-09-17 12:10:14 -0600

    1st tester for player chasing alogorithm, need fix

--- src/main.js
+++ src/main.js
@@ -22,16 +22,16 @@ function preload() {
     this.load.image('bg', require("./assets/Background.jpg"));
     this.load.image('crab', require("./assets/crab.png"));
     this.load.image('DriftBottle', require("./assets/DriftBottle.png"));
+    this.load.image('JellyFish', require("./assets/JellyFish.png"))
 }
 
 function create() {
     gameState.active = true;
+    gameState.isPlayerDiving = false;
+    gameState.isPlayerOnPlatform = false;
 
     gameState.background = this.add.image(400, 350, 'bg');
     gameState.cursors = this.input.keyboard.createCursorKeys();
-
-    gameState.road_1 = this.add.rectangle(config.width / 2, 225, config.width, 250);
-    gameState.road_1 = this.add.rectangle(config.width / 2, 525, config.width, 250);
 
     gameState.player = this.add.image(config.width / 2, config.height - 25, 'frog');
     gameState.enemies = this.add.group();
@@ -42,7 +42,7 @@ function create() {
     const platLanesEven = [8, 10];
     const platLanesOdd = [7, 9, 11];
 
-    function createEnemy() {
+    function createEnemyBeach() {
         if(!gameState.active) return null;
         const curr_lane = this.lane;
         const pos_y = config.height - (25 + (curr_lane * 50));
@@ -55,12 +55,22 @@ function create() {
         else {
             start_x = 25;
         }
-        const enemy = gameState.enemies.create(start_x, pos_y, 'crab');
+        let enemy = gameState.enemies.create(start_x, pos_y, 'crab');
         enemy.speed = 2;
         enemy.setName(name);
         return enemy;
     }
 
+    function createEnemyShark(){
+        if(!gameState.active) return null;
+        let pos_y = randBtwMinMax(125, 325);
+        let start_x = randBtwMinMax(25, 785);
+        let shark = gameState.enemies.create(start_x, pos_y, 'JellyFish');
+        shark.speed = 2;
+        shark.setName("Shark");
+        return shark;
+    }
+    gameState.shark = createEnemyShark();
     function createPlatformEven() {
         if(!gameState.active) return null;
         const curr_lane = this.lane;
@@ -82,11 +92,11 @@ function create() {
         platform.speed = 1;
         return platform;
     }
-
+    
     for (let lane of enemyLanes) {
         this.time.addEvent({
             delay: Math.random() * 2000 + 1500,
-            callback: createEnemy,
+            callback: createEnemyBeach,
             callbackScope: { lane: lane },
             loop: true,
         })
@@ -109,8 +119,8 @@ function create() {
             loop: true,
         })
     }
-
-
+    
+    
 }
 
 function update() {
@@ -130,15 +140,35 @@ function update() {
         if (Phaser.Input.Keyboard.JustDown(gameState.cursors.down) && gameState.player.y < config.height - 25) {
             gameState.player.y += 50;
         }
+        
+        // Shark AI, not on platform chase the player, on platform random roaming
+        if(!gameState.isPlayerOnPlatform){
+            console.log("Shark: Chasing player!");
+            let sharkVec2 = [gameState.shark.x, gameState.shark.y];
+            let playerVec2 = [gameState.player.x, gameState.player.y];
+            let direcVec = [playerVec2[0] - sharkVec2[0], playerVec2[1] - sharkVec2[1]];
 
+            let unitDirecVec = calcUnitVector(direcVec);
+            gameState.shark.x += Math.ceil(unitDirecVec[0] * gameState.shark.speed);
+            gameState.shark.y += Math.ceil(unitDirecVec[1] * gameState.shark.speed);
+        }
+        else{
+            console.log("Shark: Roaming!");
+            let randMove = [parseInt(randBtwMinMax(-1, 1)), parseInt(randBtwMinMax(-1, 1))];
+            gameState.shark.x += randMove[0];
+            gameState.shark.y += randMove[1];
+        }
+
+        // Beach Enemies Control
         gameState.enemies.getChildren().forEach(enemy => {
+            // Check which way the enemy is coming from, enemies from right named with pos
             if (enemy.name == "pos") {
                 enemy.x -= enemy.speed;
             }
             else {
                 enemy.x += enemy.speed;
             }
-
+            // Check colliding with player for game state
             if (checkCollision(enemy, gameState.player)) {
                 gameState.active = false;
                 this.add.text(400, 350, 'Game Over', {fontSize: '150px'}).setOrigin(0.5);
@@ -150,15 +180,19 @@ function update() {
         gameState.platformsEven.getChildren().forEach(platform => {
             platform.x += platform.speed;
             if (checkCollision(platform, gameState.player)) {
+                gameState.isPlayerOnPlatform = true;
                 gameState.player.x = platform.x;
             }
+            else{gameState.isPlayerOnPlatform = false;}
         });
 
         gameState.platformsOdd.getChildren().forEach(platform => {
             platform.x -= platform.speed;
             if (checkCollision(platform, gameState.player)) {
+                gameState.isPlayerOnPlatform = true;
                 gameState.player.x = platform.x;
             }
+            else{gameState.isPlayerOnPlatform = false;}
         });
     }
 }
@@ -169,7 +203,7 @@ function update() {
  *              B   -   Second obj to check collision with
  *  Return:     True for collision, False no collision
  */
-function checkCollision(A, B) {
+function  checkCollision(A, B) {
     let A_Left_X = A.x - 25;
     let A_Right_X = A.x + 25;
     let A_Top_Y = A.y - 25;
@@ -201,14 +235,23 @@ function removeOutOfBoundObj() {
 }
 
 /*
- *  calcNormal(vec2d):
+ *  calcUnitVector(vec2d):
  *  Param:      vec2d   -   input 2d vector to be normalized
  *  Return:     The result normalized vector
  */
-function calcNormal(vec2d) {
+function calcUnitVector(vec2d) {
     result = [vec2d[0], vec2d[1]];
     result[0] = vec2d[0] / Math.sqrt(vec2d[0] * vec2d[0] + vec2d[1] * vec2d[1]);
     result[1] = vec2d[1] / Math.sqrt(vec2d[0] * vec2d[0] + vec2d[1] * vec2d[1]);
     return result;
 }
+
+/*
+ *  randBtwMinMax(min, max):
+ *  Desc:       Get a random number between min and max input
+ */
+function randBtwMinMax(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
 
